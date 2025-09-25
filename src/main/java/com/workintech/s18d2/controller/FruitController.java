d8ab56bf8ee01c6849e9a60baa4032e00f3763b3
S18D2 <sezerin.kucuk@gmail.com>
2025-09-25 12:39:10 +0300

    xxxx

--- src/main/java/com/workintech/s18d2/controller/FruitController.java
+++ src/main/java/com/workintech/s18d2/controller/FruitController.java
@@ -3,7 +3,9 @@ package com.workintech.s18d2.controller;
 import com.workintech.s18d2.dao.FruitResponse;
 import com.workintech.s18d2.entity.Fruit;
 import com.workintech.s18d2.services.FruitService;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -31,11 +33,15 @@ public class FruitController {
         return  new FruitResponse("id succeed",fruitService.getById(id));
     }
     @GetMapping("/name/{name}")
-    public List<Fruit> getByName(String name){
+    public List<Fruit> getByName(@Size (min=2,max=45,message = "xxxx")@PathVariable("name") String name){
         return fruitService.searchByName(name);
     }
     @PostMapping
     public Fruit save(@Validated @RequestBody Fruit fruit){
         return fruitService.save(fruit);
+    }
+    @DeleteMapping("/{id}")
+    public Fruit delete(@NotNull @Positive @PathVariable long id){
+        return  fruitService.delete(id);
     }
 }
--- src/main/java/com/workintech/s18d2/controller/VegetableController.java
+++ src/main/java/com/workintech/s18d2/controller/VegetableController.java
@@ -1,11 +1,11 @@
 package com.workintech.s18d2.controller;
 
-import com.workintech.s18d2.dao.FruitResponse;
 import com.workintech.s18d2.dao.VegetableResponse;
-import com.workintech.s18d2.entity.Fruit;
 import com.workintech.s18d2.entity.Vegetable;
 import com.workintech.s18d2.services.VegetableService;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -33,11 +33,14 @@ public class VegetableController {
         return  new VegetableResponse("id succeed",vegetableService.getById(id));
     }
     @GetMapping("/name/{name}")
-    public List<Vegetable> getByName(String name){
+    public List<Vegetable> getByName(@Size(min=2,max=45,message = "xxxx")@PathVariable("name") String name){
         return vegetableService.searchByName(name);
     }
     @PostMapping
     public Vegetable save(@Validated @RequestBody Vegetable vegetable){
         return vegetableService.save(vegetable);
+    }
+    public Vegetable delete(@NotNull @Positive @PathVariable long id){
+        return  vegetableService.delete(id);
     }
 }
